22280118caeade20718a5ceb3a0bb5053b7a3023
DadiPallavi <dadipallavi5@gmail.com>
2026-05-19 23:54:00 +0530

    updated app.py

--- day2/app.py
+++ day2/app.py
@@ -17,4 +17,31 @@ with signup:
         email=st.text_input("Email")
         password=st.text_input("Password",type="password")
         btn=st.form_submit_button("SignUp")
+        if btn:
+
+            try:
+                # ---------------- INSERT ----------------
+                query = """
+                INSERT INTO users(name, email, password)
+                VALUES(%s, %s, %s)
+                """
+
+                values = (name, email, password)
+
+                cursor.execute(query, values)
+                conn.commit()
+
+                st.success("Signup Successful 🎉")
+
+                # ---------------- FETCH DATA ----------------
+                st.subheader("All Users")
+
+                cursor.execute("SELECT * FROM users")
+                users = cursor.fetchall()
+
+                st.dataframe(users)
+
+            except Exception as e:
+                st.error("Something went wrong")
+                st.write(e)
         
