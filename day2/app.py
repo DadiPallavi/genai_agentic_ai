c50d823ab9f0a14f4898d9f54d1dc692875aea9b
DadiPallavi <dadipallavi5@gmail.com>
2026-05-19 22:30:53 +0530

    First commit

---
+++ day2/app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+#from db_c import conn,cursor
+login,signup=st.tabs(
+    ["Login","Signup"]
+)
+with login:
+    st.header("Login")
+    with st.form("Login_Form"):
+        email=st.text_input("Email")
+        password=st.text_input("Password",type="password")
+        btn=st.form_submit_button("Login")
+
+with signup:
+    st.header("Signup")
+    with st.form("SignUp_Form"):
+        name=st.text_input("Name")
+        email=st.text_input("Email")
+        password=st.text_input("Password",type="password")
+        btn=st.form_submit_button("SignUp")
+        
